861d9abbf057233e4571b24b68b65e96a8232e30
unbreakable.ry <rjaypenafiel1@gmail.com>
2025-03-30 13:39:15 +0800

    Add files via upload

---
+++ C++ DICTIONARY.cxx
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+	
+	cout << "Enter your word: ";
+    cin >> word;
+
+    if (word == "abandon") {
+        cout << "to leave or to give up";
+    } else if (word == "abbreviate") {
+        cout << "to shorten words, names, etc.";
+    } else if (word == "benefit") {
+        cout << "profit/advantage";
+    } else if (word == "buffalo") {
+        cout << "large wild ox or cow";
+    } else if (word == "chitchat") {
+        cout << "gossip/talk";
+    } else if (word == "cytology") {
+        cout << "study of cells";
+    } else if (word == "dibble") {
+        cout << "a tool used to make holes in the ground for planting";
+    } else if (word == "ditty") {
+        cout << "little song/simple song";
+    } else if (word == "enigma") {
+        cout << "mystery/puzzle";
+    } else if (word == "flagon") {
+        cout << "large round container for liquids";
+    } else if (word == "gait") {
+        cout << "manner of walking";
+    } else if (word == "heliport") {
+        cout << "place where helicopters land and take off";
+    } else if (word == "incite") {
+        cout << "to encourage";
+    } else if (word == "joggle") {
+        cout << "to move rapidly/nervously";
+    } else if (word == "knoll") {
+        cout << "small hill";
+    } else if (word == "leash") {
+        cout << "strap/cord to keep a dog in control";
+    } else if (word == "mallet") {
+        cout << "large wooden hammer";
+    } else if (word == "nape") {
+        cout << "back of the neck";
+    } else if (word == "odontology") {
+        cout << "study of teeth";
+    } else if (word == "pitfall") {
+        cout << "trap/danger";
+    } else if (word == "quilt") {
+        cout << "padded cover for a bed";
+    } else if (word == "replete") {
+        cout << "full and satisfied";
+    } else if (word == "sheen") {
+        cout << "brilliant shining surface";
+    } else if (word == "twig") {
+        cout << "little branch";
+    } else if (word == "undermine") {
+        cout << "to weaken";
+    } else if (word == "vial") {
+        cout << "small glass bottle";
+    } else if (word == "whorl") {
+        cout << "coiled/spiral shape";
+    } else if (word == "xenophobia") {
+        cout << "hatred, dislike, or fear of foreigners";
+    } else if (word == "yolk") {
+        cout << "yellow part of an egg";
+    } else if (word == "zoology") {
+        cout << "study of animals";
+    } else {
+        cout << "No word is found";
+    }
+
+    return 0;
+}
+	  
